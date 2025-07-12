b186eb34b1bbe06cb747b1593617dab4028be00a
Ian Branum <ian.branum@timbergrove.com>
2025-07-12 17:39:29 +0200

    removing api key from repo :-/

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
 
-const supabaseUrl = 'https://bjgyflblgdiaclnlksui.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqZ3lmbGJsZ2RpYWNsbmxrc3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxNTgzMzAsImV4cCI6MjA2NzczNDMzMH0.ej5_C1GB2OIt4v5b2in0OZPSToSqr2SmkiQ0nX6ghvU';
+const supabaseUrl = Constants.expoConfig?.extra?.SUPABASE_URL;
+const supabaseAnonKey = Constants.expoConfig?.extra?.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
